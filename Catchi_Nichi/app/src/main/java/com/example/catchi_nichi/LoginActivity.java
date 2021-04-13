549b9c7f0c3dbedd36df2586367ae06e26be9f38
leejihyun <leejudy1017@naver.com>
2021-04-13 11:12:20 +0900

    kakaologin_server연동

--- Catchi_Nichi/app/src/main/java/com/example/catchi_nichi/LoginActivity.java
+++ Catchi_Nichi/app/src/main/java/com/example/catchi_nichi/LoginActivity.java
@@ -1,29 +1,60 @@
 package com.example.catchi_nichi;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
 
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
 import com.kakao.network.ErrorResult;
+import com.kakao.network.NetworkTask;
 import com.kakao.usermgmt.ApiErrorCode;
 import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.MeV2ResponseCallback;
 import com.kakao.usermgmt.response.MeV2Response;
 import com.kakao.util.exception.KakaoException;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.ClientProtocolException;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.impl.client.HttpClientBuilder;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import cz.msebera.android.httpclient.util.EntityUtils;
 
 public class LoginActivity extends AppCompatActivity {
+    private String myResult;
 
     private SessionCallback sessionCallback;
 
@@ -103,13 +134,47 @@ public class LoginActivity extends AppCompatActivity {
             Log.e("Info", "Info:null");
 
         for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance(String.valueOf(result));
-                md.update(signature.toByteArray());
-                Log.d("Info", String.valueOf(result));
-            } catch (NoSuchAlgorithmException e) {
-                Log.d("Info", "Info 오류 : " + signature, e);
-            }
+            String snsId = String.valueOf(result.getId());
+            String email =  String.valueOf(result.getKakaoAccount().getEmail());
+            String[] low =result.getKakaoAccount().getAgeRange().getValue().split("~");
+            int age = (Integer.parseInt(low[0])+Integer.parseInt(low[1]))/2;
+            String gender = String.valueOf(result.getKakaoAccount().getGender());
+
+            Log.d("snsId", snsId);
+            Log.d("email", email);
+            Log.d("age", String.valueOf(age));
+            Log.d("gender", gender);
+            String url =  "http://10.0.2.2:8001/user/kakao";
+
+
+            ContentValues values = new ContentValues();
+            values.put("snsId", snsId);
+            values.put("email", email);
+            values.put("age", age);
+            values.put("gender", gender);
+
+            NetworkTask networkTask = new NetworkTask(url, values);
+            networkTask.execute();
+
+        }
+    }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        String url;
+        ContentValues values;
+
+        NetworkTask(String url, ContentValues values){
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String result;
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values);
+            return result; // 결과가 여기에 담깁니다. 아래 onPostExecute()의 파라미터로 전달됩니다.
         }
     }
 
--- Catchi_Nichi/app/src/main/java/com/example/catchi_nichi/MainActivity.java
+++ Catchi_Nichi/app/src/main/java/com/example/catchi_nichi/MainActivity.java
@@ -9,21 +9,17 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    String strNickname, strProfile;
+    String snsId, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tvNickname = findViewById(R.id.tvNickname);
-        TextView tvProfile = findViewById(R.id.tvProfile);
 
         Intent intent = getIntent();
-        strNickname = intent.getStringExtra("name");
-        strProfile = intent.getStringExtra("profile");
+        snsId = intent.getStringExtra("snsId");
+        email = intent.getStringExtra("email");
 
-        tvNickname.setText(strNickname);
-        tvProfile.setText(strProfile);
     }
 }
